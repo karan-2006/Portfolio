7a0c12f0bd7b1ce9ad9ad7ec4de570d5ba05c6bb
Karan Kumar <karansunil2006@gmail.com>
2024-10-23 16:35:17 +0530

    Resume.jsx

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 function Resume() {
 
     return(
-
+        <>
         <div className="container-fluid py-5 text-light">
         <center>
         <h3 className="display-3 text-primary">My Resume</h3>
@@ -10,50 +10,53 @@ function Resume() {
         <hr className="w-25"/>
         </center>
         <div id="carouselExampleCaptions" className="carousel slide">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
-      <div className="card bg-dark text-light d-none d-md-block">
-        <div className=" card-body ms-5 ps-5">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+            {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
+            <div className="card bg-dark text-light d-none d-md-block">
+              <div className=" card-body">
+                <h5 className="card-title">First slide label</h5>
+                <p className="card-text">Some representative placeholder content for the first slide.</p>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item">
+            {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
+            <div className="card bg-dark text-light d-none d-md-block">
+              <div className=" card-body">
+              <h5 className="card-title">Second slide label</h5>
+              <p className="card-text">Some representative placeholder content for the second slide.</p>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item">
+            {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
+            <div className="card bg-dark text-light d-none d-md-block">
+              <div className=" card-body">
+                <h5 className="card-title">Third slide label</h5>
+                <p className="card-text">Some representative placeholder content for the third slide.</p>
+              </div>
+            </div>
+          </div>
+          {/*<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+          </button>*/}
+        </div>
         </div>
       </div>
-    </div>
-    <div className="carousel-item">
-      {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
-      <div className="card bg-dark text-light d-none d-md-block">
-        <div className=" card-body ms-5 ps-5">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-        </div>
-      </div>
-    </div>
-    <div className="carousel-item">
-      {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
-      <div className="card bg-dark text-light d-none d-md-block">
-      <div className=" card-body ms-5 ps-5">
-      <h5>Third slide label</h5>
-      <p>Some representative placeholder content for the third slide.</p>
-      </div>
-      </div>
-    </div>
-  </div>
-  {/*<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>*/}
-</div>
-      </div>
+      </>
+  
+      
     )
     
 }
