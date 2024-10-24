6de48bbe66c2357ff5addcc00ef7bc9054c2f6ee
Karan Kumar <karansunil2006@gmail.com>
2024-10-24 14:59:16 +0530

    Update Resume.jsx

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -10,11 +10,11 @@ function Resume() {
         <hr className="w-25"/>
         </center>
         <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-          {/*<div className="carousel-indicators">
+          <div className="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>*/}
+          </div>
           <div className="carousel-inner">
             <div className="carousel-item active">
             {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
