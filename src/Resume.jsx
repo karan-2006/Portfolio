764ea01f6317cb5469d08e23df91bd946fc7ed11
Karan Kumar <karansunil2006@gmail.com>
2024-10-24 19:20:46 +0530

    Update Resume.jsx

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -17,7 +17,7 @@ function Resume() {
           </div>
           <div className="carousel-inner">
             <div className="carousel-item active">
-            {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
+            
             <div className="card bg-secondary bg-gradient text-light d-none d-md-block m-5 p-5">
                 <h4 className="card-title text-info">2016-2017</h4>
                 <h5 className="card-title">SSLC - AVICHI Higher Seondary School</h5>
@@ -26,7 +26,7 @@ function Resume() {
             </div>
           </div>
           <div className="carousel-item">
-            {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
+            
             <div className="card bg-secondary  bg-gradient text-light d-none d-md-block m-5 p-5">
               <h4 className="card-title text-info">2018-2019</h4>
               <h5 className="card-title">HSC - AVICHI Higher Seondary School</h5>
@@ -35,7 +35,7 @@ function Resume() {
             </div>
           </div>
           <div className="carousel-item">
-            {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
+            
             <div className="card bg-secondary bg-gradient text-light d-none d-md-block m-5 p-5">
                 <h4 className="car-title text-info">2019-2023</h4>
                 <h5 className="card-title">B.E - DMI College of Engineering</h5>
@@ -43,14 +43,7 @@ function Resume() {
                  sed do <br /> eiusmod tempor incididunt ut labore et dolore magna</p>
             </div>
           </div>
-          {/*<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-          </button>*/}
+          
         </div>
         </div>
       </div>
