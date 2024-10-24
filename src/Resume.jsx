22a4ba4c9d6fd803ed4103e5707b4f2f3b9a9b0e
Karan Kumar <karansunil2006@gmail.com>
2024-10-24 15:50:30 +0530

    Resume.jsx

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -21,7 +21,7 @@ function Resume() {
             <div className="card bg-secondary bg-gradient text-light d-none d-md-block m-5 p-5">
                 <h4 className="card-title text-info">2016-2017</h4>
                 <h5 className="card-title">SSLC - AVICHI Higher Seondary School</h5>
-                <p className="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                <p className="card-text text-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                  sed do <br /> eiusmod tempor incididunt ut labore et dolore magna</p>
             </div>
           </div>
@@ -30,7 +30,7 @@ function Resume() {
             <div className="card bg-secondary  bg-gradient text-light d-none d-md-block m-5 p-5">
               <h4 className="card-title text-info">2018-2019</h4>
               <h5 className="card-title">HSC - AVICHI Higher Seondary School</h5>
-              <p className="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              <p className="card-text text-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                  sed do <br /> eiusmod tempor incididunt ut labore et dolore magna</p>
             </div>
           </div>
@@ -39,7 +39,7 @@ function Resume() {
             <div className="card bg-secondary bg-gradient text-light d-none d-md-block m-5 p-5">
                 <h4 className="car-title text-info">2019-2023</h4>
                 <h5 className="card-title">B.E - DMI College of Engineering</h5>
-                <p className="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                <p className="card-text text-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                  sed do <br /> eiusmod tempor incididunt ut labore et dolore magna</p>
             </div>
           </div>
