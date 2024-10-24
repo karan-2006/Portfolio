59a93db87c0aa9dfe471f59b9502403ecd23ad33
Karan Kumar <karansunil2006@gmail.com>
2024-10-24 15:35:57 +0530

    update Resume.jsx

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -9,7 +9,7 @@ function Resume() {
         <h6 className="display-6 text-secondary">Lorem ipsum dolor sit amet</h6>
         <hr className="w-25"/>
         </center>
-        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleCaptions" className="carousel carousel-dark slide" data-bs-ride="carousel">
           <div className="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -18,8 +18,8 @@ function Resume() {
           <div className="carousel-inner">
             <div className="carousel-item active">
             {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
-            <div className="card bg-dark text-light d-none d-md-block m-5 p-5">
-                <h4 classname="card-title">2016-2017</h4>
+            <div className="card bg-secondary text-light d-none d-md-block m-5 p-5">
+                <h4 classname="card-title text-info">2016-2017</h4>
                 <h5 className="card-title">SSLC - AVICHI Higher Seondary School</h5>
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                  sed do <br /> eiusmod tempor incididunt ut labore et dolore magna</p>
@@ -27,8 +27,8 @@ function Resume() {
           </div>
           <div className="carousel-item">
             {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
-            <div className="card bg-dark text-light d-none d-md-block m-5 p-5">
-              <h4 className="card-title">2018-2019</h4>
+            <div className="card bg-secondary text-light d-none d-md-block m-5 p-5">
+              <h4 className="card-title text-info">2018-2019</h4>
               <h5 className="card-title">HSC - AVICHI Higher Seondary School</h5>
               <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                  sed do <br /> eiusmod tempor incididunt ut labore et dolore magna</p>
@@ -36,8 +36,8 @@ function Resume() {
           </div>
           <div className="carousel-item">
             {/*<img src={slide} className="d-block" style={{width:"100%",height:"300px"}} alt="..."/>*/}
-            <div className="card bg-dark text-light d-none d-md-block m-5 p-5">
-                <h4 className="car-title">2019-2023</h4>
+            <div className="card bg-secondary text-light d-none d-md-block m-5 p-5">
+                <h4 className="car-title text-info">2019-2023</h4>
                 <h5 className="card-title">B.E - DMI College of Engineering</h5>
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                  sed do <br /> eiusmod tempor incididunt ut labore et dolore magna</p>
