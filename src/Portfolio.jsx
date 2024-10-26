c5e7d26a67cb6673bebb199dede0f72d795b8d02
Karan Kumar <karansunil2006@gmail.com>
2024-10-26 23:50:54 +0530

    Update File and Images

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,156 +1,109 @@
-import waterDrop from './assets/Water-drop.jpg'
-
+import React, { useState } from 'react';
+import proImg from './assets/Ceramics.jpg'
+import fiJet from './assets/Jet.jpg'
+import watDrop from './assets/Water-drop.jpg'
 
 
 function Portfolio() {
+  const [selectedProject, setSelectedProject] = useState(null);
 
-    return(
+  const projects = [
+    {
+      title: 'POTTERY',
+      description: 'DESIGNING',
+      image: proImg,
+      link: 'https://your-project-link.com',
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+        title: 'FIGHTER JET',
+        description: 'DESIGNING',
+        image: fiJet,
+        link: 'https://your-project-link.com',
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      },
+      {
+        title: 'Pottery Project',
+        description: 'Designing',
+        image: watDrop,
+        link: 'https://your-project-link.com',
+        content:'',
+      },
+      {
+        title: 'Pottery Project',
+        description: 'Designing',
+        image: watDrop,
+        link: 'https://your-project-link.com',
+        content:'',
+      },
+    // Add more projects here
+  ];
+
+  const handleProjectClick = (project) => {
+    setSelectedProject(project);
+  };
+
+  const handleModalClose = () => {
+    setSelectedProject(null);
+  };
+
+  return (
+    <div className="container-fluid">
+        <center>
+            <h3 className="display-3 text-primary">Portfolio</h3>
+            <h6 className="display-6 text-secondary">See My Works - Lorem ipsum dolor</h6>
+            <hr className="w-25" />
+        </center>
+        {/* ... button section ... */}
+
         <div className="container-fluid">
-            <center>
-                <h3 className="display-3 text-primary">Portfolio</h3>
-                <h6 className="display-6 text-secondary">See My Works - Lorem ipsum dolor</h6>
-                <hr className="w-25"/>
-                <div className="container">
-                    <button className="btn btn-outline-light m-5" type="button">ALL</button>
-                    <button className="btn btn-outline-light m-5" type="button">PROJECT</button>
-                    <button className="btn btn-outline-light m-5" type="button">WEBSITE</button>  
+            <div className="row">
+                {projects.map((project, index) => (
+                <div className="col" key={index}>
+                    <div className="card text-bg-dark" onClick={() => handleProjectClick(project)}>
+                        <img src={project.image} style={{ width: '100%', height: '200px' }} className="card-img" alt={project.title} />
+                        <div className="card-img-overlay bg-dark bg-opacity-75 w-75 h-75 mt-4 ms-5">
+                            <h5 className="card-title">{project.title}</h5>
+                            <p className="card-text text-warning">{project.description}</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col">
-                            <div className="card text-bg-dark">
-                                <img src={waterDrop} className="card-img" alt="..."/>
-                                <div id="cardone" className="container" data-bs-toggle="modal" data-bs-target="#exampleModal1">
-                                    <div className="card-img-overlay bg-dark bg-opacity-75 my-2 mx-3">
-                                        <h5 className="card-title text-light">Pottery</h5>
-                                        <p className="card-text text-warning">Designing</p>
-                                    </div>
-                                </div>
-                            </div>
+            ))}
+            </div>
+        </div>
+
+        {selectedProject && (
+            <div className="modal show" style={{ display: 'block' }}>
+                <div className="modal-dialog">
+                    <div className="modal-content bg-dark">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{selectedProject.title}</h5>
+                            <button type="button" className="btn-close btn-close-white" onClick={handleModalClose} aria-label="Close"></button>
                         </div>
-                        <div className="modal bg-dark bg-opacity-25 fade w-100" id="exampleModal1" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div className="modal-dialog">
-                                <div className="modal-content bg-dark">
-                                    <div className="modal-header">
-                                        <h1 className="modal-title fs-5" id="exampleModalLabel">Pottery</h1>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <p className='modal-text px-5 py-5'>Lorem Ipsum is simply dummy text
-                                              of the printing and typesetting industry. Lorem Ipsum
-                                               has been the industry's standard dummy text
-                                              ever since the 1500s.</p>
-                                    </div>
-                                    <button type="button" className="btn btn-outline-light">
-                                        <a className=' text-secondary text-decoration-none' href="">View Project</a>
-                                    </button>
+                        <div className="modal-body">
+                            <div className="row">
+                                <div className="col">
+                                    <img src={selectedProject.image} style={{ width: '100%', height: '200px' }} className="modal-img" alt={selectedProject.title} />
                                 </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card text-bg-dark">
-                                <img src={waterDrop} className="card-img" alt="..."/>
-                                <div id="cardtwo" className="container" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                    <div className="card-img-overlay bg-dark bg-opacity-75 my-2 mx-3">
-                                        <h5 className="card-title text-light">Fighter Jet</h5>
-                                        <p className="card-text text-warning">Designing</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="modal bg-dark bg-opacity-25 fade w-100" id="exampleModal2" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div className="modal-dialog">
-                                <div className="modal-content bg-dark">
-                                <div className="modal-header">
-                                    <h1 className="modal-title fs-5" id="exampleModalLabel">Fighter Jet</h1>
-                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div className="row">
-                                <div className="col-1 px-3 py-5">
-                                <img className="px-1" src={waterDrop} width={100} height={200} alt="..."/>
-                                </div>
-                                <div className="col-11">
-                                    <div className="modal-body">
-                                        
-                                        <p className='modal-text px-5 py-5'>Lorem Ipsum is simply dummy text
-                                              of the printing and typesetting industry. Lorem Ipsum
-                                               has been the industry's standard dummy text
-                                              ever since the 1500s.</p>
-                                        
-                                        <button type="button" className="btn btn-outline-light">
-                                            <a className=' text-secondary text-decoration-none' href="">View Project</a>
+                                <div className="col">
+                                    <blockquote>
+                                        <p className='text-warning'>{selectedProject.description}</p>
+                                        <h6 className='text-secondary py-3'>{selectedProject.content}</h6>
+                                        <button type="button" className='btn btn-outline-light'>
+                                            <a className='text-decoration-none' href={selectedProject.link} target="_blank" rel="noopener noreferrer">
+                                                View Project
+                                            </a>
                                         </button>
-                                    </div>
-                                    </div>
-                                    </div>
-                            </div>
-                        </div>
-                        </div>
-                        <div className="col">
-                            <div className="card text-bg-dark">
-                                <img src={waterDrop} className="card-img" alt="..."/>
-                                <div id="cardthree" className="container" data-bs-toggle="modal" data-bs-target="#exampleModal3">
-                                    <div className="card-img-overlay bg-dark bg-opacity-75 my-2 mx-3">
-                                        <h5 className="card-title text-light">Project Title</h5>
-                                        <p className="card-text text-warning">Designing</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="modal bg-dark bg-opacity-25 fade w-100" id="exampleModal3" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div className="modal-dialog">
-                                <div className="modal-content bg-dark">
-                                    <div className="modal-header">
-                                        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <p className='modal-text px-5 py-5'>Lorem Ipsum is simply dummy text
-                                              of the printing and typesetting industry. Lorem Ipsum
-                                               has been the industry's standard dummy text
-                                              ever since the 1500s.</p>
-                                    </div>
-                                    <button type="button" className="btn btn-outline-light">
-                                        <a className=' text-secondary text-decoration-none' href="">View Project</a>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card text-bg-dark">
-                                <img src={waterDrop} className="card-img" alt="..."/>
-                                <div id="cardfour" className="container" data-bs-toggle="modal" data-bs-target="#exampleModal4">
-                                    <div className="card-img-overlay bg-dark bg-opacity-75 my-2 mx-3">
-                                        <h5 className="card-title text-light">Project Title</h5>
-                                        <p className="card-text text-warning">Designing</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="modal bg-dark bg-opacity-25 fade w-100" id="exampleModal4" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div className="modal-dialog">
-                                <div className="modal-content bg-dark">
-                                    <div className="modal-header">
-                                        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <p className='modal-text px-5 py-5'>Lorem Ipsum is simply dummy text
-                                              of the printing and typesetting industry. Lorem Ipsum
-                                               has been the industry's standard dummy text
-                                              ever since the 1500s.</p>
-                                    </div>
-                                        <button type="button" className="btn btn-outline-light">
-                                            <a className=' text-secondary text-decoration-none' href="">View Project</a>
-                                        </button>
+                                    </blockquote>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </center>
-        </div>
-    )
-    
+            </div>
+        )}
+    </div>
+  );
 }
-export default Portfolio
+
+export default Portfolio;
