d2b2b44eaa9aec0774b4efd4091cf8016c425097
Karan Kumar <karansunil2006@gmail.com>
2024-10-28 14:54:29 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
     <div className=" my-5 py-5">
     <div className="container-fluid position-relative my-5 py-5" id="home">
     <h5 className="text-primary pt-5">Hi There! I'm</h5>
-    <h5 className="text-success display-5 py-3">Full Stack Developer</h5>
+    <h5 className="text-success display-5 py-4">Full Stack Developer</h5>
     <p className="text-secondary">Lorem Ipsum is simply dummy text of the printing and <br />
      typesetting industry.Lorem Ipsum has been the industry's <br /> standard dummy text ever</p>
-     <button className="btn btn-outline-danger my-3" type="button">Download CV</button>
+     <button className="btn btn-outline-danger my-5" type="button">Download CV</button>
     </div>
     </div>
 
